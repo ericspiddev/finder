9c85926535a9f2cae3a5ac66264adbd396f7c142
Eric Spidle <ericspidledev@gmail.com>
2025-11-28 16:34:56 -0500

    debug: rework debug so dbg level is accessible outside of module

--- lib/finder_debug.lua
+++ lib/finder_debug.lua
@@ -6,23 +6,22 @@ function finder_debug:new(debug_level, log_function)
     return setmetatable(obj, self)
 end
 
-DEBUG_LEVELS = {DEBUG = 0, INFO = 1, WARNING = 2, ERROR = 3, OFF = 4}
+finder_debug.DEBUG_LEVELS = {DEBUG = 0, INFO = 1, WARNING = 2, ERROR = 3, OFF = 4}
 
 function finder_debug:debug_print(msg, variable)
-    vim.print("debg var is " ..vim.inspect(variable))
-    self:finder_print(DEBUG_LEVELS.DEBUG, "[FINDER DBG]: ", msg, variable)
+    self:finder_print(self.DEBUG_LEVELS.DEBUG, "[FINDER DBG]: ", msg, variable)
 end
 
 function finder_debug:info_print(msg, variable)
-    self:finder_print(DEBUG_LEVELS.INFO, "[FINDER INFO]: ", msg, variable)
+    self:finder_print(self.DEBUG_LEVELS.INFO, "[FINDER INFO]: ", msg, variable)
 end
 
 function finder_debug:warning_print(msg, variable)
-    self:finder_print(DEBUG_LEVELS.WARNING, "[FINDER WARN]: ", msg, variable)
+    self:finder_print(self.DEBUG_LEVELS.WARNING, "[FINDER WARN]: ", msg, variable)
 end
 
 function finder_debug:error_print(msg, variable)
-    self:finder_print(DEBUG_LEVELS.ERROR, "[FINDER ERR]: ", msg, variable)
+    self:finder_print(self.DEBUG_LEVELS.ERROR, "[FINDER ERR]: ", msg, variable)
 end
 
 function finder_debug:finder_print(level, prefix, msg, variable)
